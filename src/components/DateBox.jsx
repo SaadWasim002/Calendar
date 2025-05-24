b50d1fc04966bdde33be501d23bbcc60fb20235f
SaadWasim02 <saadwasim444@gmail.com>
2025-05-24 06:13:22 +0530

    final

--- src/components/DateBox.jsx
+++ src/components/DateBox.jsx
@@ -1,10 +1,12 @@
 import { useDrop } from 'react-dnd';
-import { format, isSameDay } from 'date-fns';
+import { format, isBefore, isSameDay, isSameMonth, startOfMonth } from 'date-fns';
 import EventItem from './EventItem';
 
 const DateBox = ({
     day, currentMonth, onClick, events ,onDrop , onEventClick   
 }) => {
+    const isCurrentMonth = isSameMonth(day, currentMonth);
+    const isPrevMonth = isBefore(day, startOfMonth(currentMonth));
     const [{ isOver }, drop] = useDrop(() => ({
         accept: 'EVENT',
         drop: (item) => onDrop(item.id, day),
@@ -14,12 +16,16 @@ const DateBox = ({
         <>
             <div
                 ref={drop}
-                className={`day-cell ${!isSameDay(day, new Date()) ? '' : 'today'}`}
+                className={`day-cell 
+                    ${!isSameDay(day, new Date()) ? '' : 'today'} 
+                    ${!isCurrentMonth ? 'other-month' : ''}
+                    ${isPrevMonth ? 'prev-month' : ''}`}
                 onClick={onClick}
             >
                 <div className="date-label">
                     {format(day, 'd')}
                 </div>
+                <div className="events-container">
                 {events.map(event =>
                      <EventItem
                         key={event.id}
@@ -27,6 +33,7 @@ const DateBox = ({
                         onClick={() => onEventClick(event)}
                     />
                 )}
+                </div>
             </div>
         </>
     )
